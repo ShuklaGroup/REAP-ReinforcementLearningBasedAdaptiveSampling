7e1ac09b20d82d1108c2949d5bd28e8fbd0d5a48
Zahra Shamsi <zshamsi2@illinois.edu>
2017-04-04 18:59:51 -0500

    Update mockSimulation.py

--- toyPotential/mockSimulation.py
+++ toyPotential/mockSimulation.py
@@ -1,1 +1,17 @@
 class mockSimulation:
+        r = #number of rounds
+        s = # length of simulations
+        N = # number of parallel simulations
+        ## public
+        def runNxtRound():
+                
+                
+        
+        ## private
+        def adaptiveSampling():
+                
+                
+        def creatPotentioal():
+                
+                
+                
