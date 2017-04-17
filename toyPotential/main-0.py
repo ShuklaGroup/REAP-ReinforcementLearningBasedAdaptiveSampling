7906f411df32762f5620344edad36f0c7af3720f
Zahra Shamsi <zshamsi2@illinois.edu>
2017-04-17 13:18:57 -0500

    Update main-0.py

--- toyPotential/main-0.py
+++ toyPotential/main-0.py
@@ -3,6 +3,7 @@ import RLSim
 X_0 = [1,0.5,1.4]
 Y_0 = [1,1.2,1.7]
 
+N = len(X_0)
 # run first round of simulation
 trj1 = RLSim.mockSimulation.run(X_0, Y_0)
 
@@ -16,10 +17,10 @@ trj1_Sp_theta = RLSim.mockSimulation.map(trj1_Sp)
 W_0 = [1/2, 1/2]
 
 # update weigths 
-W_1 = RLSim.mockSimulation.updateW(trj1_Sp_theta, W_0)
+W_1 = RLSim.mockSimulation.updateW(trj1_Sp_theta, prior_weigths = W_0)
 
 # get new starting points (in theta domain) using new reward function based on updated weigths (W_1)
-newPoints_index = RLSim.mockSimulation.adSampling(trj1_Sp_theta, weigths = W_1, method = 'RL')
+newPoints_index = RLSim.mockSimulation.findStarting(trj1_Sp_theta, starting_n = N ,weigths = W_1, method = 'RL')
 
 X_1, Y_1 = trj1_Sp[newPoints_index]
 
