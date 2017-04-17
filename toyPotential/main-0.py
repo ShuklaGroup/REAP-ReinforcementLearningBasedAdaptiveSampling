2978bc1e4f772d089ccb96b712cb492b210ca9de
Zahra Shamsi <zshamsi2@illinois.edu>
2017-04-17 13:11:17 -0500

    Create main-0.py

---
+++ toyPotential/main-0.py
@@ -0,0 +1,26 @@
+import RLSim
+
+X_0 = [1,0.5,1.4]
+Y_0 = [1,1.2,1.7]
+
+# run first round of simulation
+trj1 = RLSim.mockSimulation.run(X_0, Y_0)
+
+# choose states with min count or newly discovered
+trj1_Sp = RLSim.mockSimulation.PreSamp(trj1)
+
+# map coordinate space to reaction coorinates space
+trj1_Sp_theta = RLSim.mockSimulation.map(trj1_Sp)
+
+# initialize the weights vector
+W_0 = [1/2, 1/2]
+
+# update weigths 
+W_1 = RLSim.mockSimulation.updateW(trj1_Sp_theta, W_0)
+
+# get new starting points (in theta domain) using new reward function based on updated weigths (W_1)
+newPoints_index = RLSim.mockSimulation.adSampling(trj1_Sp_theta, weigths = W_1, method = 'RL')
+
+X_1, Y_1 = trj1_Sp[newPoints_index]
+
+trj2 = RLSim.mockSimulation.run(X_1, Y_1)
