241a38f8260d24e1a6eaeaf5a2c2e10a9c4f90be
Zahra Shamsi <zshamsi2@illinois.edu>
2017-06-27 19:36:35 -0500

    Update RLSim.py

--- MDSimulation/Alanine dipeptide/RLSim.py
+++ MDSimulation/Alanine dipeptide/RLSim.py
@@ -6,13 +6,7 @@ class mockSimulation:
                 self.r = 1#number of rounds
                 self.s = 1# length of simulations
                 self.N = 1# number of parallel simulations
-                self.msm = None
-                
-        def run_multipleSim(self):
-                return True
-        def runNxtRound(self):
-                return True
-                
+                self.msm = None           
         
         ## private
         def PreAll(self, trj):
@@ -25,11 +19,6 @@ class mockSimulation:
                 """
                 import numpy as np
                 comb_trj = np.concatenate(trj)
-
-                #for theta in range(len(trj)):
-                #        comb_trj.append(np.concatenate(np.concatenate(trj[theta])))
-                #trj_Sp = np.array(comb_trj) # pick all
-                
                 return trj_Sp
 
 
@@ -103,43 +92,14 @@ class mockSimulation:
                         trj_Ps_theta_2.append(trj_Ps_theta[:,theta_index])
                 return trj_Ps_theta_2
                 """
-
-        def reward_state(self, S, theta_mean, theta_std, W_):
-                """
-                with direction
-                """
-                r_s = 0
-                for k in range(len(W_)):
-                        """
-                        r_s = r_s + W_[k]*(abs(S[k] - theta_mean[k])/theta_std[k]) #No direction
-                        """
-                        if (S[k] - theta_mean[k]) < 0: 
-                                r_s = r_s + W_[k][0]*(abs(S[k] - theta_mean[k])/theta_std[k])
-                        else:
-                                r_s = r_s + W_[k][1]*(abs(S[k] - theta_mean[k])/theta_std[k])
-                        
-                return r_s
  
-        def reward_state_noDir(self, S, theta_mean, theta_std, W_):
+        def reward_state(self, S, theta_mean, theta_std, W_):
                 # no direction
                 r_s = 0
                 for k in range(len(W_)):
                         r_s = r_s + W_[k]*(abs(S[k] - theta_mean[k])/theta_std[k]) #No direction
                 return r_s
         
-        def reward_state_withoutStd(self, S, theta_mean, theta_std, W_):
-                
-                r_s = 0
-                for k in range(len(W_)):
-                        
-                        r_s = r_s + W_[k]*(abs(S[k] - theta_mean[k])) # no direction
-                        """
-                        if (S[k] - theta_mean[k]) < 0: 
-                                r_s = r_s + W_[k][0]*(abs(S[k] - theta_mean[k]))
-                        else:
-                                r_s = r_s + W_[k][1]*(abs(S[k] - theta_mean[k]))
-                        """
-                return r_s
 
 
         def updateStat(self, trj_Sp_theta):      
@@ -171,52 +131,8 @@ class mockSimulation:
                 R = np.sum(np.array(r))
                 return R
                 
-        
-        
+
         def updateW(self, trj_Sp_theta, W_0):
-                """
-                update weigths 
-                prior_weigths = W_0
-                with considering direction
-                """
-                def fun(x):
-                        global trj_Sp_theta_z
-                        global n_ec
-                        import numpy as np
-                        x = np.array(x)
-                        W_0 = x.reshape(n_ec, 2)
-                        # W_0 = x
-                        r_0 = self.reward_trj(trj_Sp_theta, W_0)
-                        return -1*r_0     
-                
-                import numpy as np
-                from scipy.optimize import minimize
-                
-                global trj_Sp_theta_z 
-                global n_ec
-                
-                trj_Sp_theta_z = trj_Sp_theta
-                alpha = 0.2
-                delta = alpha
-                cons = ({'type': 'eq',
-                          'fun' : lambda x: np.array([np.sum(x)-1])},
-                         {'type': 'ineq',
-                          'fun' : lambda x: np.array([np.min(x)])},
-                         {'type': 'ineq',
-                          'fun' : lambda x: np.array([np.abs(np.sum(x-x0))+delta])})
-
-                #x0 = W_0
-                x0 = np.concatenate(W_0)
-                res = minimize(fun, x0, constraints=cons)
-
-                x = res.x
-                x = x/(np.sum(x))
-                W = x.reshape(n_ec, 2)
-                
-                #W = x
-                return W
- 
-        def updateW_noDir(self, trj_Sp_theta, W_0):
                 """
                 update weigths 
                 prior_weigths = W_0
@@ -282,9 +198,7 @@ class mockSimulation:
                 return newPoints
         
         
-        def creatPotentioal(self):
-                return True
-                
+               
         def run(self, production_steps = 200, start='ala2_1stFrame.pdb', production='ala2_production.pdb'): #### ?!!!!!!!!!!!!!!!!
                 from __future__ import print_function
                 from simtk.openmm import app
@@ -406,58 +320,68 @@ class mockSimulation:
                                 myfile.close()
                 return
 
-        def collect_times(self):
+
+
+       
+####################################
+
+        def updateW_withDir(self, trj_Sp_theta, W_0):
+                """
+                update weigths 
+                prior_weigths = W_0
+                with considering direction
+                """
+                def fun(x):
+                        global trj_Sp_theta_z
+                        global n_ec
+                        import numpy as np
+                        x = np.array(x)
+                        W_0 = x.reshape(n_ec, 2)
+                        # W_0 = x
+                        r_0 = self.reward_trj(trj_Sp_theta, W_0)
+                        return -1*r_0     
+                
                 import numpy as np
-                T_len = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30] # lenght of trajectories
-                T_n = range(10,1010,10) # number of trajectories
-                N=10
-                l = len(T_len)
-                n = len(T_n)
-                time = np.empty([l, n])
-                for i in range(l):
-                        for j in range(n):
-                                T_len1 = T_len[i]
-                                T_n1 = T_n[j]
-                                r=T_n1/N
-                                N=10
-                                s=T_len1
-                                t = np.load('foldTime_'+'r'+str(r)+'N'+str(N)+'s'+str(s)+'.npy')
-                                print(t)
-                                time[i][j] = t.item()
-                np.savetxt('times.txt', time)
-                return time
+                from scipy.optimize import minimize
+                
+                global trj_Sp_theta_z 
+                global n_ec
+                
+                trj_Sp_theta_z = trj_Sp_theta
+                alpha = 0.2
+                delta = alpha
+                cons = ({'type': 'eq',
+                          'fun' : lambda x: np.array([np.sum(x)-1])},
+                         {'type': 'ineq',
+                          'fun' : lambda x: np.array([np.min(x)])},
+                         {'type': 'ineq',
+                          'fun' : lambda x: np.array([np.abs(np.sum(x-x0))+delta])})
 
-def pltTimes_folding(times, filename='pcolormesh_timeReachingActive.png'):
-        """
-        Parameters
-        ----------
-        times :
-                2D array of (number of trajectory(x or j axis) x each trajectory length(y or i axis)
-                containing relative entropy of different simulations on MSM with respect to original MSM
-        output:
-                plots and saves the plot of 2D array
-        """
+                #x0 = W_0
+                x0 = np.concatenate(W_0)
+                res = minimize(fun, x0, constraints=cons)
 
-        import matplotlib.pyplot as plt
-        from matplotlib.colors import LogNorm
-        font = {'family':'Times New Roman', 'size': 22}
-        plt.rc('font', **font)
-
-        T_len = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
-        T_n = range(10,1000,10)
-
-        fig, ax = plt.subplots(1)
-        p = ax.pcolormesh(times, norm=LogNorm(vmin=83, vmax=8300)) # Values used for ww
-        xticks = range(len(T_n))
-        yticks = range(len(T_len))
-
-        T_n1 = [10, 200,400,600,800,1000]
-        ax.set_xticklabels(T_n1)
-        cbar = fig.colorbar(p)
-        ax.set(xlabel='Number of trajectories', ylabel=r'Scaled trajectory Length/$\tau$')
-        ax.set_xlim([0,99])
-        ax.set_ylim([0,30])
-
-        fig.set_size_inches(9, 7)
-        fig.savefig(filename, dpi=300)
-        fig.show()
+                x = res.x
+                x = x/(np.sum(x))
+                W = x.reshape(n_ec, 2)
+                
+                #W = x
+                return W
+        
+        
+        def reward_state_withDir(self, S, theta_mean, theta_std, W_):
+                """
+                with direction
+                """
+                r_s = 0
+                for k in range(len(W_)):
+                        """
+                        r_s = r_s + W_[k]*(abs(S[k] - theta_mean[k])/theta_std[k]) #No direction
+                        """
+                        if (S[k] - theta_mean[k]) < 0: 
+                                r_s = r_s + W_[k][0]*(abs(S[k] - theta_mean[k])/theta_std[k])
+                        else:
+                                r_s = r_s + W_[k][1]*(abs(S[k] - theta_mean[k])/theta_std[k])
+                        
+                return r_s        
+        
