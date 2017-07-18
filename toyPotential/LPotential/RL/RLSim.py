b4642cf45042322005700f795b195ce0b711973c
Zahra Shamsi <zshamsi2@illinois.edu>
2017-07-18 12:10:34 -0500

    Update and rename toyPotential/LPotential/RLSim.py to toyPotential/LPotential/RL/RLSim.py

--- toyPotential/LPotential/RL/RLSim.py
+++ toyPotential/LPotential/RL/RLSim.py
@@ -311,16 +311,6 @@ class mockSimulation:
                 for j in range(1,5):
                         V1 =  V1 + AA[j]*np.exp(aa[j]*np.square(xx-XX[j]) + bb[j]*(xx-XX[j])*(yy-YY[j]) + cc[j]*np.square(yy-YY[j]))
 
-                #fig = plt.figure()
-                #ax = fig.add_subplot(111)
-                
-                #plt.axvline(x=self.theta_mean[0])
-                #plt.axhline(y=self.theta_mean[1])
-
-                #plt.xlabel('x')
-                #plt.ylabel('y')
-
-
 ##### Main loop
 
                 trj_x = []
@@ -349,12 +339,6 @@ class mockSimulation:
                         y = y - h*dVy + np.sqrt(h*mu)*np.random.randn(1,n1)
                         trj_x.append(x) 
                         trj_y.append(y)
-                        #for j in range(len(trj_x)):
-                        #        ax.plot(trj_x[j], trj_y[j], 'o', color='w')
-                                
-                        #ax.plot(x,y, 'o', color='r')
-                        #fig.canvas.draw()
-                        #print('zz')
                         
                 return trj_x, trj_y    
 
@@ -375,6 +359,7 @@ class mockSimulation:
 
                 # temperature ###?!!!
                 mu=9
+                mu =3
                 # parameter used as stopping criterion
                 tol1 = 1e-7
 
@@ -384,8 +369,8 @@ class mockSimulation:
                 n1 = 25
                 n1 = len(inits_x)
                 # time-step (limited by the ODE step on line 83 & 84 but independent of n1)
-                #h = 1e-4
-                h = 5e-5
+                h = 1e-4
+                #h = 5e-5
                 # end points of the initial string
                 # notice that they do NOT have to be at minima of V -- the method finds
                 # those automatically
@@ -406,9 +391,7 @@ class mockSimulation:
                 aa = [-2, -20, -20, -20, -20] # inverse radius in x
                 bb = [0, 0, 0, 0, 0] # radius in xy
                 cc = [-20, -20, -2, -20, -20] # inverse radius in y
-                AA = 3*[-200, -120, -200, -80, -80] # strength
-                #AA = [-200, -120, -200, -80, -80] # strength
-
+                AA = 30*[-200, -120, -200, -80, -80] # strength
 
                 XX = [1, 0, 0, 0, 0.4] # center_x
                 YY = [0, 0, 1, 0.4, 0] # center_y
@@ -416,7 +399,6 @@ class mockSimulation:
                 zxx = np.mgrid[-1:2.51:0.01]
                 zyy = np.mgrid[-1:2.51:0.01]
                 xx, yy = np.meshgrid(zxx, zyy)
-
 
                 V1 = AA[0]*np.exp(aa[0] * np.square(xx-XX[0]) + bb[0] * (xx-XX[0]) * (yy-YY[0]) +cc[0]*np.square(yy-YY[0]))
                 for j in range(1,5):
@@ -441,16 +423,16 @@ class mockSimulation:
                         # calculation of the x and y-components of the force, dVx and dVy respectively
                         ax.contourf(xx,yy,np.minimum(V1,200), 40)
                         ee = AA[0]*np.exp(aa[0]*np.square(x-XX[0])+bb[0]*(x-XX[0])*(y-YY[0])+cc[0]*np.square(y-YY[0]))
-                        dVx = (2*aa[0]*(x-XX[0])+bb[0]*(y-YY[0]))*ee
-                        dVy = (bb[0]*(x-XX[0])+2*cc[0]*(y-YY[0]))*ee
+                        dVx = (aa[0]*(x-XX[0])+bb[0]*(y-YY[0]))*ee
+                        dVy = (bb[0]*(x-XX[0])+cc[0]*(y-YY[0]))*ee
                         for j in range(1,5):
                                 ee = AA[j]*np.exp(aa[j]*np.square(x-XX[j])+bb[j]*(x-XX[j])*(y-YY[j])+cc[j]*np.square(y-YY[j]))
-                                dVx = dVx + (2*aa[j]*(x-XX[j])+bb[j]*(y-YY[j]))*ee
-                                dVy = dVy + (bb[j]*(x-XX[j])+2*cc[j]*(y-YY[j]))*ee
+                                dVx = dVx + (aa[j]*(x-XX[j])+bb[j]*(y-YY[j]))*ee
+                                dVy = dVy + (bb[j]*(x-XX[j])+cc[j]*(y-YY[j]))*ee
                         x0 = x
                         y0 = y
-                        x = x - h*dVx + np.sqrt(2*h*mu)*np.random.randn(1,n1)
-                        y = y - h*dVy + np.sqrt(2*h*mu)*np.random.randn(1,n1) 
+                        x = x - h*dVx + np.sqrt(h*mu)*np.random.randn(1,n1)
+                        y = y - h*dVy + np.sqrt(h*mu)*np.random.randn(1,n1) 
                         trj_x.append(x) 
                         trj_y.append(y)
                         for j in range(len(trj_x)):
@@ -458,7 +440,6 @@ class mockSimulation:
                                 
                         ax.plot(x,y, 'o', color='r')
                         fig.canvas.draw()
-                        #print('zz')
                         
                 return trj_x, trj_y          
 
