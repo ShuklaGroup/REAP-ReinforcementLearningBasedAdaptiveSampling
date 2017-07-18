abb3800617fccb0b520129ee76a6d5dfb0dc1b5b
Zahra Shamsi <zshamsi2@illinois.edu>
2017-07-18 12:13:49 -0500

    Create SLMain.py

---
+++ toyPotential/LPotential/SL/SLMain.py
@@ -0,0 +1,28 @@
+import LCSim as lc
+import numpy as np 
+
+X_0 = [1.1]
+Y_0 = [0.01]
+
+
+N = len(X_0) # number of parallel runs 
+nstepmax = 15*26*24
+nstepmax = 3*5*700+3*10
+# run first round of simulation
+my_sim = lc.mockSimulation()
+
+
+# first round
+trj1 = my_sim.run_noPlt([X_0, Y_0], nstepmax = nstepmax)
+trj1 = my_sim.PreAll(trj1)
+
+trjs = trj1
+
+
+count = 1
+
+
+my_sim.pltFinalPoints(trj1)
+np.save('Trjs', trj1)	
+np.save('count_all', count)	
+
