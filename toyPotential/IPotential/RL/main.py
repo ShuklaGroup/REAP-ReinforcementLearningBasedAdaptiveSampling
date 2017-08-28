80c1a0a9a236811a8661edb4abbdd81b5ac79f81
Zahra Shamsi <zshamsi2@illinois.edu>
2017-08-28 18:48:54 -0500

    Update main.py

--- toyPotential/IPotential/RL/main.py
+++ toyPotential/IPotential/RL/main.py
@@ -2,10 +2,8 @@ import RLSim as rl
 import numpy as np 
 
 
-X_0 = [0.5, 0.5, 0.5]
-X_0 = [0.5]
-Y_0 = [0.6]
-
+X_0 = [0.3, 0.3, 0.3, 0.3, 0.3]
+Y_0 = [0.6, 0.6, 0.6, 0.6, 0.6]
 
 N = len(X_0) # number of parallel runs 
 
@@ -18,19 +16,19 @@ Ws = [] # series of weights
 # first round
 round = 0
 oldTrjs = [X_0, Y_0]
-trj1 = my_sim.run([X_0, Y_0], round, oldTrjs, nstepmax = 40)
+trj1 = my_sim.run([X_0, Y_0], round, oldTrjs, nstepmax = 20)
 #trj1 = my_sim.run_noPlt([X_0, Y_0], nstepmax = 10)
 trj1 = my_sim.PreAll(trj1)
 
 trjs = trj1
-trj1_Sp = my_sim.PreSamp(trj1, starting_n = N, myn_clusters = 20) # pre analysis
-trj1_Sp_theta = my_sim.map(trj1_Sp)
+trj1_Sp = my_sim.PreSamp(trj1, starting_n = N, myn_clusters = 5) # pre analysis
+trj1_Sp_theta = np.array(my_sim.map(trj1_Sp))
 newPoints = my_sim.findStarting(trj1_Sp_theta, trj1_Sp, W_0, starting_n = N , method = 'RL')
 
 trjs_theta = trj1_Sp_theta
 
 trjs_Sp_theta = trj1_Sp_theta
-for round in range(10):
+for round in range(100):
 	# updates the std and mean 
 
 	my_sim.updateStat(trjs_theta) # based on all trajectories
@@ -42,7 +40,7 @@ for round in range(10):
 	
 	#trj1 = my_sim.run_noPlt(newPoints, nstepmax = 5)
 	oldTrjs = trjs
-	trj1 = my_sim.run(newPoints, round, oldTrjs ,nstepmax = 40)
+	trj1 = my_sim.run(newPoints, round, oldTrjs ,nstepmax = 20)
 	trj1 = my_sim.PreAll(trj1) # 2 x all points of this round
 
 	com_trjs = []
@@ -53,7 +51,7 @@ for round in range(10):
 	trjs = np.array(com_trjs)
 	trjs_theta = np.array(my_sim.map(trjs))
 	
-	trjs_Sp = my_sim.PreSamp(trjs, starting_n = N, myn_clusters = 10)
+	trjs_Sp = my_sim.PreSamp(trjs, starting_n = N)
 	trjs_Sp_theta = np.array(my_sim.map(trjs_Sp))
   
 
@@ -63,5 +61,5 @@ for round in range(10):
 
 
 print(Ws)
-np.save('w', Ws)
-np.save('trjs_theta', trjs_theta)
+np.save('w_2', Ws)
+np.save('trjs_theta_2', trjs_theta)
