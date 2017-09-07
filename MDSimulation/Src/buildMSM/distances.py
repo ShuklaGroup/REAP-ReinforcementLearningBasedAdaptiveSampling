5255425d6b389bb1d82b688b35e5337fe62df870
Zahra Shamsi <zshamsi2@illinois.edu>
2017-09-06 20:09:21 -0500

    Create distances.py

---
+++ MDSimulation/Src/buildMSM/distances.py
@@ -0,0 +1,26 @@
+import glob
+import numpy as np
+import mdtraj as md
+
+topFile = 'csrc_inactive.pdb'
+top = md.load(topFile).topology
+
+# aloop
+a1 = top.select('name CD and resid 50 and resname GLU')
+b1 = top.select('name NZ and residq 35 and resname LYS')
+
+a2 = a1
+b2 = top.select('name CZ and residq 149 and resname ARG')
+pairs = [[a1[0],b1[0]],[a2[0],b2[0]]]
+
+
+#############
+for file in glob.glob('*/*.lh5'):
+	print(file)
+	t = md.load(file)
+	# distances
+    dist1 = md.compute_distances(t,pairs)
+	
+	np.save(file.replace('.lh5','_KE-RE.npy'), dist1)
+    
+    
