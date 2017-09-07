6db2ae5bfee22f745d74cb736eff353bdc6f5c96
Zahra Shamsi <zshamsi2@illinois.edu>
2017-09-07 17:10:52 -0500

    Update distances.py

--- MDSimulation/Src/buildMSM/distances.py
+++ MDSimulation/Src/buildMSM/distances.py
@@ -7,10 +7,10 @@ top = md.load(topFile).topology
 
 # aloop
 a1 = top.select('name CD and resid 50 and resname GLU')
-b1 = top.select('name NZ and residq 35 and resname LYS')
+b1 = top.select('name NZ and resid 35 and resname LYS')
 
 a2 = a1
-b2 = top.select('name CZ and residq 149 and resname ARG')
+b2 = top.select('name CZ and resid 149 and resname ARG')
 pairs = [[a1[0],b1[0]],[a2[0],b2[0]]]
 
 
@@ -19,7 +19,7 @@ for file in glob.glob('*/*.lh5'):
 	print(file)
 	t = md.load(file)
 	# distances
-    dist1 = md.compute_distances(t,pairs)
+	dist1 = md.compute_distances(t,pairs)
 	
 	np.save(file.replace('.lh5','_KE-RE.npy'), dist1)
     
