0ff16f861fc76492f3c0646050d35cbb503fb252
Zahra Shamsi <zshamsi2@illinois.edu>
2017-09-07 19:16:55 -0500

    Update SLmain.py

--- MDSimulation/Src/SL/SLmain.py
+++ MDSimulation/Src/SL/SLmain.py
@@ -1,25 +1,28 @@
 # python 2.7 MSMbuilder 2.8
 
 from scipy import io as sio
-sio.mmwrite('transmat.mtx', msm.transmat_)
-
-
-
-import LCSim as lc
+import SLSim as sl
 import numpy as np 
 
+# inital state
 S_0 = [0]
 
-N = len(X_0) # number of parallel runs 
-nstepmax = 15*26*24
-nstepmax = 3*5*700+3*10
+N = len(S_0) # number of parallel runs 
+nstepmax = 15
 
 # run simulation
-my_sim = lc.mockSimulation()
+my_sim = sl.mockSimulation()
+my_sim.tp = sio.mmread('tProb.mtx')
+my_sim.x = np.load('Gens_aloopRMSD.npy')
+my_sim.y = np.load('Gens_y_deltaDist.npy')
+
 trj1 = my_sim.run(S_0, nstepmax = nstepmax)
 trj1 = my_sim.PreAll(trj1)
 
-trjs = trj1
+trjs = trj1[0]
 
-my_sim.pltPoints(trj1)
+
+trj_x, trj_y = my_sim.map(trjs)
+my_sim.pltPoints(trj_x, trj_y)
+
 np.save('Trjs', trj1)	
