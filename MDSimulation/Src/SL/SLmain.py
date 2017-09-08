56ba914942733e529768c13a4abbb530ed72a6cf
Zahra Shamsi <zshamsi2@illinois.edu>
2017-09-08 16:20:09 -0500

    Update SLmain.py

--- MDSimulation/Src/SL/SLmain.py
+++ MDSimulation/Src/SL/SLmain.py
@@ -5,16 +5,18 @@ import SLSim as sl
 import numpy as np 
 
 # inital state
-S_0 = [0]
+S_0 = [0] # inactive
 
+#S_0 = [1728] # active
 N = len(S_0) # number of parallel runs 
-nstepmax = 15
-
+nstepmax = 20000
+print(nstepmax*0.005)
 # run simulation
 my_sim = sl.mockSimulation()
 my_sim.tp = sio.mmread('tProb.mtx')
 my_sim.x = np.load('Gens_aloopRMSD.npy')
 my_sim.y = np.load('Gens_y_deltaDist.npy')
+my_sim.mapping = np.load('map_Macro2micro.npy')
 
 trj1 = my_sim.run(S_0, nstepmax = nstepmax)
 trj1 = my_sim.PreAll(trj1)
@@ -26,3 +28,5 @@ trj_x, trj_y = my_sim.map(trjs)
 my_sim.pltPoints(trj_x, trj_y)
 
 np.save('Trjs', trj1)	
+
+
