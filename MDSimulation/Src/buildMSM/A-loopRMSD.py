0cf56cbe4c28036bab44b084947562af124af4c8
Zahra Shamsi <zshamsi2@illinois.edu>
2017-09-12 11:35:51 -0500

    Update A-loopRMSD.py

--- MDSimulation/Src/buildMSM/A-loopRMSD.py
+++ MDSimulation/Src/buildMSM/A-loopRMSD.py
@@ -23,3 +23,26 @@ for file in glob.glob('*/*.lh5'):
 	np.save(file.replace('.lh5','_aloopRMSD.npy'), r)
     
     
+#########
+import glob
+import numpy as np
+import mdtraj as md
+
+file = 'MSM_traj_csrc_100microsecs.pdb'
+refFile = 'csrc_inactive.pdb'
+
+trj = md.load(file)
+top = trj.topology
+
+# aloop
+aloop_select = 'resid 144 to 164'
+aloop_atomIndex = top.select(aloop_select)
+
+# ref aloop
+aloop_ref = md.load(refFile).atom_slice(aloop_atomIndex)
+
+t2 = t.atom_slice(aloop_atomIndex)
+r = md.rmsd(t2, aloop_ref)
+
+np.save('MSM_traj_csrc_100microsecs__aloopRMSD.npy', r)
+
