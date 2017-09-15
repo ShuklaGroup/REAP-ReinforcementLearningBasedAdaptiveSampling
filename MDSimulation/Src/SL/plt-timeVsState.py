66b21a0150caf628f90fe2f7b9cbc08860b7b523
Zahra Shamsi <zshamsi2@illinois.edu>
2017-09-14 20:09:53 -0500

    Create plt-timeVsState.py

---
+++ MDSimulation/Src/SL/plt-timeVsState.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+z = np.load('Trjs.npy')[0]
+
+states = []
+data = []
+
+for i in range(len(z)):
+    states.append(z[i])
+    states = np.unique(states)
+    n_discoveredS = len(states)
+    time = i
+    data.append([time, n_discoveredS])
+    
+np.save('n_discoveredS_time.npy', data)    
+
+import matplotlib.pyplot as plt
+plt.plot(data)
+plt.show()
+
