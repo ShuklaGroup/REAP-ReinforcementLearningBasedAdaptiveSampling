f268f833ea7f33d20682ba714c1cbeb3752459dc
Zahra Shamsi <zshamsi2@illinois.edu>
2017-10-27 11:37:44 -0500

    Update LCmain.py

--- MDSimulation/Src/LC/LCmain.py
+++ MDSimulation/Src/LC/LCmain.py
@@ -11,17 +11,17 @@ N = len(S_0) # number of parallel runs
 nstepmax = 80
 print('Simulation length: ', nstepmax*0.005)
 # W_0 = [[1/4, 1/4], [1/4, 1/4]] # initial geuss of weights for + - in x and y directions
-W_0 = [0.5, 0.5]
-Ws = [] # series of weights
-Ws.append(W_0)
-print('Weight', W_0)
-# run simulation
+# W_0 = [0.5, 0.5]
+# Ws = [] # series of weights
+# Ws.append(W_0)
+# print('Weight', W_0)
+
+# Simulation setup
 my_sim = rl.mockSimulation()
 my_sim.tp = sio.mmread('tProb.mtx')
 my_sim.x = np.load('Gens_aloopRMSD.npy')
 my_sim.y = np.load('Gens_y_deltaDist.npy')
 my_sim.mapping = np.load('map_Macro2micro.npy')
-
 
 #### first round
 trj1 = my_sim.run(S_0, nstepmax = nstepmax) # 1 x 1 x n_frames
@@ -29,19 +29,19 @@ trj1 = my_sim.PreAll(trj1) # 1 x 1 x n_frames
 trjs = trj1[0] # 1 x n_frames
 trj1_Sp = my_sim.PreSamp_MC(trjs, N = 20) # pre analysis # 1 x n_samples
 trj1_Sp_theta = np.array(my_sim.map(trj1_Sp)) # [trjx, trjy]
-newPoints = my_sim.findStarting(trj1_Sp_theta, trj1_Sp, W_0, starting_n = N , method = 'RL') # needs 2 x something
+newPoints = my_sim.findStarting(trj1_Sp_theta, trj1_Sp, starting_n = N , method = 'LC') # needs 2 x something ###### CHANGE
 trjs_theta = trj1_Sp_theta
 trjs_Sp_theta = trj1_Sp_theta
 
 
 for round in range(500):
 	# updates the std and mean 
-	my_sim.updateStat(trjs_theta) # based on all trajectories
+	#my_sim.updateStat(trjs_theta) # based on all trajectories
 
-	W_1 = my_sim.updateW(trjs_Sp_theta, W_0) # important
-	W_0 = W_1
-	Ws.append(W_0)
-	print('Weight', W_0)
+	#W_1 = my_sim.updateW(trjs_Sp_theta, W_0) # important
+	#W_0 = W_1
+	#Ws.append(W_0)
+	#print('Weight', W_0)
 	
 	oldTrjs = trjs
 	trj1 = my_sim.run(newPoints, nstepmax = 20)
@@ -53,13 +53,10 @@ for round in range(500):
 	
 	trjs_Sp = my_sim.PreSamp_MC(trjs, N = 20)
 	trjs_Sp_theta = np.array(my_sim.map(trjs_Sp))
-	newPoints = my_sim.findStarting(trjs_Sp_theta, trjs_Sp, W_1, starting_n = N , method = 'RL')
+	newPoints = my_sim.findStarting(trjs_Sp_theta, trjs_Sp, starting_n = N , method = 'LC') #### CHANGE !!!!!
 
 
 #my_sim.pltPoints(trjs_theta, trjs_Sp_theta, newPoints, round, weights=Ws)
-
 my_sim.pltPoints(trjs_theta[0], trjs_theta[1])
-print(Ws)
-np.save('w_2', Ws)
-np.save('trjs_theta_2', trjs_theta)
+np.save('trjs_theta_LC', trjs_theta)
 
