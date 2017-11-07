6a0cfb47f9f3cb3014fb2f5dad0fd9587c838af3
Zahra Shamsi <zshamsi2@illinois.edu>
2017-11-06 19:35:39 -0600

    Update mRLmain.py

--- MDSimulation/Src/RL-multiDimension/mRLmain.py
+++ MDSimulation/Src/RL-multiDimension/mRLmain.py
@@ -6,20 +6,26 @@ import numpy as np
 
 # inital state
 S_0 = [0] # inactive
+n_op = 4 # number of order parameters
 
 N = len(S_0) # number of parallel runs 
 nstepmax = 80
 print('Simulation length: ', nstepmax*0.005)
 # W_0 = [[1/4, 1/4], [1/4, 1/4]] # initial geuss of weights for + - in x and y directions
-W_0 = [0.5, 0.5]
-Ws = [] # series of weights
+# W_0 = [0.5, 0.5] 
+W_0 = 1/n_op*np.ones(n_op)
+
+Ws = [] # time series of weights
 Ws.append(W_0)
 print('Weight', W_0)
 # run simulation
 my_sim = rl.mockSimulation()
 my_sim.tp = sio.mmread('tProb.mtx')
-my_sim.x = np.load('Gens_aloopRMSD.npy')
-my_sim.y = np.load('Gens_y_deltaDist.npy')
+my_sim.x1 = np.load('Gens_aloopRMSD.npy')
+my_sim.x2 = np.load('Gens_y_deltaDist.npy')
+!!my_sim.x3 = np.load('')
+!!my_sim.x4 = np.load('')
+
 my_sim.mapping = np.load('map_Macro2micro.npy')
 
 #### first round
@@ -27,8 +33,10 @@ trj1 = my_sim.run(S_0, nstepmax = nstepmax) # 1 x 1 x n_frames
 trj1 = my_sim.PreAll(trj1) # 1 x 1 x n_frames
 trjs = trj1[0] # 1 x n_frames
 trj1_Sp = my_sim.PreSamp_MC(trjs, N = 20) # pre analysis # 1 x n_samples
-trj1_Sp_theta = np.array(my_sim.map(trj1_Sp)) # [trjx, trjy]
-newPoints = my_sim.findStarting(trj1_Sp_theta, trj1_Sp, W_0, starting_n = N , method = 'RL') # needs 2 x something
+
+trj1_Sp_theta = np.array(my_sim.map(trj1_Sp)) # [trjx1, trjx2, ...]
+
+!!newPoints = my_sim.findStarting(trj1_Sp_theta, trj1_Sp, W_0, starting_n = N , method = 'RL') # needs 2 x something
 trjs_theta = trj1_Sp_theta
 trjs_Sp_theta = trj1_Sp_theta
 
