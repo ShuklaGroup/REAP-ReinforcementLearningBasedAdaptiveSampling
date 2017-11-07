d97c2f079aaee878bebc19ebe73de9e2f2b67af1
Zahra Shamsi <zshamsi2@illinois.edu>
2017-11-06 19:41:05 -0600

    Update mRLmain.py

--- MDSimulation/Src/RL-multiDimension/mRLmain.py
+++ MDSimulation/Src/RL-multiDimension/mRLmain.py
@@ -34,17 +34,16 @@ trj1 = my_sim.PreAll(trj1) # 1 x 1 x n_frames
 trjs = trj1[0] # 1 x n_frames
 trj1_Sp = my_sim.PreSamp_MC(trjs, N = 20) # pre analysis # 1 x n_samples
 
-trj1_Sp_theta = np.array(my_sim.map(trj1_Sp)) # [trjx1, trjx2, ...]
-
-!!newPoints = my_sim.findStarting(trj1_Sp_theta, trj1_Sp, W_0, starting_n = N , method = 'RL') # needs 2 x something
+trj1_Sp_theta = np.array(my_sim.map(trj1_Sp)) # [trjx1, trjx2, ...] 
+newPoints = my_sim.findStarting(trj1_Sp_theta, trj1_Sp, W_0, starting_n = N , method = 'RL') # needs 2 x something   ### Should work fine
 trjs_theta = trj1_Sp_theta
 trjs_Sp_theta = trj1_Sp_theta
 
 for round in range(500):
 	# updates the std and mean 
-	my_sim.updateStat(trjs_theta) # based on all trajectories
+	my_sim.updateStat(trjs_theta) # based on all trajectories  ### Should work fine
 
-	W_1 = my_sim.updateW(trjs_Sp_theta, W_0) # important
+	W_1 = my_sim.updateW(trjs_Sp_theta, W_0) # important ## Should change with differet OP
 	W_0 = W_1
 	Ws.append(W_0)
 	print('Weight', W_0)
@@ -67,4 +66,11 @@ np.save('w_2', Ws)
 np.save('trjs_theta_2', trjs_theta)
 
 
+
+
+#################
+
+## Should change with differet OP 
+# map
+# updateW
 
