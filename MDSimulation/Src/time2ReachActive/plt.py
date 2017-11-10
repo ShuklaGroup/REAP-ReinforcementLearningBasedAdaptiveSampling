9d392c1064234fe54657e3d0d1f039b5bfec7611
Zahra Shamsi <zshamsi2@illinois.edu>
2017-11-10 12:13:54 -0600

    Update plt.py

--- MDSimulation/Src/time2ReachActive/plt.py
+++ MDSimulation/Src/time2ReachActive/plt.py
@@ -1,5 +1,11 @@
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
+import pylab as plt
+import seaborn as sns
+
+plt.rcParams.update({'font.size':20})
+plt.rc('xtick', labelsize=20)
+plt.rc('ytick', labelsize=20)
 
 
 LC = np.load('LC/LC-activationTimes.npy')
@@ -11,8 +17,19 @@ RL = 0.005*RL
 SL = np.load('SL/SL-activationTimes.npy')
 SL = 0.005*SL
 
-fig, axes = plt.subplots(nrows=1, ncols=1)
-axes.boxplot([LC, RL])
-plt.savefig('fig')
+sns.set_style("whitegrid")
+
+
+ax1= sns.boxplot(data=[RL, LC, SL], palette='spectral',width=.28)
+#ax1 = sns.swarmplot(data=[LC,RL], size=6, edgecolor="black", linewidth=.9)
+
+#plt.xticks(rotation=-20)
+ax1.set(xticklabels=['REAP', 'Least Count', 'Single Long'])
+
+#plt.xlabel('Portion of landscape discovered')
+ax1.set_ylabel('Time to reach to active ('+ r'$\mu$'+'s)', fontdict={'fontsize' : 20})
+#ax1.set_yticks([0, 20, 40])
+
+plt.savefig('fig4')
 plt.show()
 
